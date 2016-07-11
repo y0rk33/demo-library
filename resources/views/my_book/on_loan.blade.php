cfd74ee05eb9178a8b42e1223b2f968db4a0a8af
y0rk33 <duyanh.ngac@gmail.com>
2016-07-11 09:26:13 +0800

    Update on_loan.blade.php

--- resources/views/my_book/on_loan.blade.php
+++ resources/views/my_book/on_loan.blade.php
@@ -15,39 +15,42 @@
 				<p>You don't have any books on loan</p>
 			</div>
 		@else
-			<table class="table table-striped table-bordered table-responsive">
-				<thead>
-				<th>Request Number</th>
-				<th>ISBN</th>
-				<th>Book Title</th>
-				<th>Status</th>
-				<th>Date of Loan</th>
-				<th>Date to Return</th>
-				<th>Fine</th>
-				</thead>
-
-				<tbody>
-				@foreach($borrow_transactions as $transaction)
-					<tr>
-						<td>{{ $transaction->request_number }}</td>
-						<td><a href="{{ url('book/'.$transaction->book->id) }}">{{ $transaction->book->isbn }}</a></td>
-						<td>
-							{{ $transaction->book->title }}, {{ $transaction->book->edition }}, {{ $transaction->book->year }}
-						</td>
-						<td>{{ $transaction->status }}</td>
-						<td>{{ $transaction->borrowed_at }}</td>
-						<td>{{ $transaction->to_be_returned_at }}</td>
-						<td>
-							@if(!empty($transaction->fine))
-								<span class="text-danger">{{ $transaction->fine->amount }}</span>
-							@else
-								<span class="text-success">No Fine</span>
-							@endif
-						</td>
-					</tr>
-				@endforeach
-				</tbody>
-			</table>
+			<div class="table-responsive">
+				<table class="table table-striped table-bordered">
+					<thead>
+						<tr>
+							<th>Request Number</th>
+							<th>ISBN</th>
+							<th>Book Title</th>
+							<th>Status</th>
+							<th>Date of Loan</th>
+							<th>Date to Return</th>
+							<th>Fine</th>
+						</tr>
+					</thead>
+					<tbody>
+					@foreach($borrow_transactions as $transaction)
+						<tr>
+							<td>{{ $transaction->request_number }}</td>
+							<td><a href="{{ url('book/'.$transaction->book->id) }}">{{ $transaction->book->isbn }}</a></td>
+							<td>
+								{{ $transaction->book->title }}, {{ $transaction->book->edition }}, {{ $transaction->book->year }}
+							</td>
+							<td>{{ $transaction->status }}</td>
+							<td>{{ $transaction->borrowed_at }}</td>
+							<td>{{ $transaction->to_be_returned_at }}</td>
+							<td>
+								@if(!empty($transaction->fine))
+									<span class="text-danger">{{ $transaction->fine->amount }}</span>
+								@else
+									<span class="text-success">No Fine</span>
+								@endif
+							</td>
+						</tr>
+					@endforeach
+					</tbody>
+				</table>
+			</div>
 		@endif
 
 	</div>
